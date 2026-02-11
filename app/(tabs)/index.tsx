2d4c94f771db8c280e80ef27521b3e31a84e9220
Bruno <bruno.mitsuhiroo@gmail.com>
2026-02-11 19:22:33 -0300

    Avatares

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+const avatar = require("../../assets/images/Avatar_Guaxinim.png");
 
 export default function Index() {
   return (
     <View style={style.container}>
-      <Text style={style.text}>Hello Expo.</Text>
+      <View style={style.section}>
+        <View style={style.avatarSection}>
+          <Image style={style.avatarImage} source={avatar} />
+        </View>
+        <View>
+          <TextInput
+            style={style.input}
+            placeholder="Quem quer gastar dinerus?..."
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,12 +29,32 @@ const style = StyleSheet.create({
   text: {
     color: "white",
   },
-  button: {
-    fontSize: 20,
-    color: "white",
-    paddingHorizontal: 20,
-    paddingVertical: 5,
+  section: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  avatarImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 25,
+    borderWidth: 3,
+    backgroundColor: "#7d7d7c",
+    borderColor: "#2e2e2d",
+  },
+  avatarSection: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "gray",
+    backgroundColor: "#8a8a8a",
+    color: "black",
+    height: 40,
+    width: 300,
+    borderWidth: 1,
+    paddingHorizontal: 10,
     borderRadius: 5,
-    backgroundColor: "gray",
+    marginTop: 10,
   },
 });
